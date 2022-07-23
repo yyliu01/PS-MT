904c3551ce9423df77d6a10f342575f9d5bec15b
Yuyuan Liu <102338056+yyliu01@users.noreply.github.com>
2022-07-23 14:24:16 +0930

    Update tensor_board.py

--- CityCode/Utils/tensor_board.py
+++ CityCode/Utils/tensor_board.py
@@ -11,7 +11,7 @@ from Utils.helpers import DeNormalize
 
 class Tensorboard:
     def __init__(self, config, online=False):
-        os.environ['WANDB_API_KEY'] = "6cde1f75fbdf236d8e89b77d313d74b40e3d8d5f"
+        os.environ['WANDB_API_KEY'] = ""
         os.system("wandb login")
         os.system("wandb {}".format("online" if online else "offline"))
         self.tensor_board = wandb.init(project=config['name'], name=config['experim_name'],
